b6b2b38cc7f0701801fa6fd31c0b109781aedea7
Elijah Southman <elijahsouthman@gmail.com>
2026-01-03 21:09:04 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, FlatList, View } from "react-native";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+
+		this.state = {
+			tasks: []
+		}
+	}
+
+	componentWillUnmount()
+	{
+	}
+
+	componentDidMount()
+	{
+		this.setState({tasks: [{length:2}, {length:1}, {length:6}] });
+	}
+
+	taskRender = ({item}) => {
+		return (<View style={[styles.task, {height:`${(item.length / 12) * 100}%`}]}>
+			<Text>{`${item.length}`}</Text>
+		</View>)
+	}
+
+	render()
+	{
+		return (<View style={styles.container}><View style={styles.subcontainer}>
+			<StatusBar></StatusBar>
+			<Text>Hi</Text>
+			<View style={{backgroundColor:"red", flex:1}}><FlatList
+				data={this.state.tasks}
+				renderItem={this.taskRender}
+				keyExtractor={(item, index) => index}
+			/></View></View>
+		</View>);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container:{
+		flex:1,
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	subcontainer:
+	{
+		width: 336,
+		height: 690,
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 16
+	},
+	task:{
+		width:300,
+		borderWidth:2,
+		borderColor:"black",
+		backgroundColor:"green"
+	}
 });
