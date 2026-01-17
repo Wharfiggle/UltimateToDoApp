9b7b5f35a972ac59264c4a50381350c484d21591
Elijah Southman <elijahsouthman@gmail.com>
2026-01-17 13:14:36 -0600

    Basic time tracker

--- App.js
+++ App.js
@@ -10,8 +10,13 @@ export default class App extends React.Component
 
 		this.state = {
 			tasks: [],
-			listHeight: 0
+			listHeight: 0,
+			timeMarkerPos: 0
 		}
+
+		this.timePeriod = {start: Date.now(), end: Date.now() + 1 * 12 * 1000}
+
+		this.timeMarkerInterval = setInterval(this.timeMarkerUpdate, 1000);
 	}
 
 	componentWillUnmount()
@@ -20,7 +25,7 @@ export default class App extends React.Component
 
 	componentDidMount()
 	{
-		this.setState({tasks: [{length:2}, {length:1}, {length:6}] });
+		this.setState({tasks: [{length:2}, {length:1}, {length:6}, {length:3}] });
 	}
 
 	//called upon rendering task list
@@ -28,16 +33,22 @@ export default class App extends React.Component
 		this.setState({ listHeight: e.nativeEvent.layout.height });
 	}
 
+	timeMarkerUpdate = () => {
+		var timeMarkerPos = this.state.listHeight * ((Date.now() - this.timePeriod.start) / (this.timePeriod.end - this.timePeriod.start));
+		timeMarkerPos = Math.max(0, Math.min(this.state.listHeight, timeMarkerPos));
+		this.setState({ timeMarkerPos: timeMarkerPos });
+	}
+
 	taskRender = ({item}) => {
 		//get sum of all task lengths
 		const total = this.state.tasks.reduce((sum, task) => sum + task.length, 0);
 
-		var heightStyle = { flex: item.length };
+		var taskStyle = styles.task;
 		if(this.state.listHeight != 0)
-			heightStyle = { height: (item.length / total) * this.state.listHeight };
+			taskStyle = {...taskStyle, height: (item.length / total) * this.state.listHeight };
 
 		return (
-			<View style={[styles.task, heightStyle]}>
+			<View style = {taskStyle}>
 				<Text>{`${item.length}`}</Text>
 			</View>
 		)
@@ -54,8 +65,9 @@ export default class App extends React.Component
 							data={this.state.tasks}
 							renderItem={this.taskRender}
 							keyExtractor={(item, index) => index}
-							ItemSeparatorComponent={(<View style={{height:2, backgroundColor:"black"}}></View>)}
+							ItemSeparatorComponent={(<View style={{height:1, backgroundColor:"black"}}></View>)}
 						/>
+						<View style={{position:"absolute", width: 100, top: this.state.timeMarkerPos - 1, height:2, backgroundColor:"red"}}></View>
 					</View>
 				</View>
 			</View>
@@ -67,7 +79,8 @@ const styles = StyleSheet.create({
 	container:{
 		flex:1,
 		justifyContent:"center",
-		alignItems:"center"
+		alignItems:"center",
+		backgroundColor:"black"
 	},
 	subcontainer:
 	{
@@ -79,8 +92,7 @@ const styles = StyleSheet.create({
 	},
 	task:{
 		width:300,
-		borderColor:"black",
-		backgroundColor:"green",
+		backgroundColor:"white",
 		justifyContent:"center",
 		alignItems:"center"
 	}
